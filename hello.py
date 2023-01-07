1a9fa7e0a9c17a928498a17222b361ebe93d0dbc
publo336 <336deidara@gmail.com>
2023-01-07 11:12:57 -0600

    hello.py

---
+++ hello.py
@@ -0,0 +1,11 @@
+print("Hello, world!")
+
+print("""Hello,
+This is the example for multiline
+in single print function using triple quotation""")
+
+# I'd like to watch a movie
+
+print("I'd like to watch a movie")
+
+# I'm fund of the saying "something"
